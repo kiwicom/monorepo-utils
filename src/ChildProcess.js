d18d07dea3f6d699b1e222e398a4eb82133be0c5
Martin Zlámal <martin.zlamal@kiwi.com>
2019-04-04 11:01:05 -0600

    Monorepo: workaround Babel transpilation bug in child process utility
    Closes: https://gitlab.skypicker.com/incubator/universe/issues/467
    
    kiwicom-source-id: 146239f917e95a0c8517e32a05d2e37de019beba

--- src/ChildProcess.js
+++ src/ChildProcess.js
@@ -2,8 +2,6 @@
 
 const nodeChildProcess = require('child_process');
 
-const cwd = process.cwd();
-
 /*::
 
 // See: https://github.com/facebook/flow/blob/af99a9e671351a83971ec880e6bfcdc96eeba2d7/lib/node.js#L275
@@ -14,6 +12,63 @@ type cp$spawnSyncOpts = {| +stdio: 'inherit' |};
 
 */
 
+const cwd = process.cwd();
+
+/**
+ * Method `executeNodeScript` is perfect when you need to fork current
+ * Node.js process and run another Node.js script by filename.
+ */
+function executeNodeScript(
+  modulePath /*: string */,
+  argsOrOptions /*: ?(string[] | cp$forkOpts) */,
+  options /*: ?cp$forkOpts */,
+) /*: child_process$ChildProcess */ {
+  const defaultOptions = { cwd, stdio: 'inherit' };
+  const child = Array.isArray(argsOrOptions)
+    ? nodeChildProcess.fork(modulePath, argsOrOptions, {
+        ...defaultOptions,
+        ...options,
+      })
+    : nodeChildProcess.fork(modulePath, {
+        ...defaultOptions,
+        ...argsOrOptions,
+        ...options,
+      });
+  return bindChildProcess(child);
+}
+
+/**
+ * Method `executeSystemCommand` is great if you need to run command on your OS.
+ * This function performs additional checks to make sure this command actually
+ * exists on your system.
+ */
+function executeSystemCommand(
+  command /*: string */,
+  argsOrOptions /*: ?(string[] | cp$spawnSyncOpts) */,
+  options /*: ?cp$spawnSyncOpts */,
+) /*: string */ {
+  // note: doesn't work on Windows
+  // escape somehow (?):
+  try {
+    nodeChildProcess.execSync(`command -v ${command}`);
+  } catch (error) {
+    error.message = `Command '${command}' doesn't exist in your OS and therefore cannot be executed.`;
+    throw error;
+  }
+  const { stdout, status } = Array.isArray(argsOrOptions)
+    ? nodeChildProcess.spawnSync(command, argsOrOptions, {
+        ...options,
+      })
+    : nodeChildProcess.spawnSync(command, {
+        ...argsOrOptions,
+        ...options,
+      });
+  process.exitCode = status;
+  return stdout !== null
+    ? stdout.toString() // stdout is available only when used without stdio:inherit (otherwise it's being streamed)
+    : stdout;
+}
+
 /**
  * This is our opinionated wrapper around `child_process` module. It exposes
  * limited API to solve all common problems when calling `child_process` from
@@ -21,60 +76,8 @@ type cp$spawnSyncOpts = {| +stdio: 'inherit' |};
  * Moreover, it has proper Flow types in contract to `execa`.
  */
 module.exports = {
-  /**
-   * Method `executeNodeScript` is perfect when you need to fork current
-   * Node.js process and run another Node.js script by filename.
-   */
-  executeNodeScript: (
-    modulePath /*: string */,
-    argsOrOptions /*: ?(string[] | cp$forkOpts) */,
-    options /*: ?cp$forkOpts */,
-  ) /*: child_process$ChildProcess */ => {
-    const defaultOptions = { cwd, stdio: 'inherit' };
-    const child = Array.isArray(argsOrOptions)
-      ? nodeChildProcess.fork(modulePath, argsOrOptions, {
-          ...defaultOptions,
-          ...options,
-        })
-      : nodeChildProcess.fork(modulePath, {
-          ...defaultOptions,
-          ...argsOrOptions,
-          ...options,
-        });
-    return bindChildProcess(child);
-  },
-
-  /**
-   * Method `executeSystemCommand` is great if you need to run command on your OS.
-   * This function performs additional checks to make sure this command actually
-   * exists on your system.
-   */
-  executeSystemCommand: (
-    command /*: string */,
-    argsOrOptions /*: ?(string[] | cp$spawnSyncOpts) */,
-    options /*: ?cp$spawnSyncOpts */,
-  ) /*: string */ => {
-    // note: doesn't work on Windows
-    // escape somehow (?):
-    try {
-      nodeChildProcess.execSync(`command -v ${command}`);
-    } catch (error) {
-      error.message = `Command '${command}' doesn't exist in your OS and therefore cannot be executed.`;
-      throw error;
-    }
-    const { stdout, status } = Array.isArray(argsOrOptions)
-      ? nodeChildProcess.spawnSync(command, argsOrOptions, {
-          ...options,
-        })
-      : nodeChildProcess.spawnSync(command, {
-          ...argsOrOptions,
-          ...options,
-        });
-    process.exitCode = status;
-    return stdout !== null
-      ? stdout.toString() // stdout is available only when used without stdio:inherit (otherwise it's being streamed)
-      : stdout;
-  },
+  executeNodeScript,
+  executeSystemCommand,
 
   // Note: we do not allow `exec` here which spawns `shell` to run the command.
 };
